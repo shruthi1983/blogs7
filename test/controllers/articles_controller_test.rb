129e1d1d1dcf709508deb0e0215850f52eb1bd3c
shruthi <shruthirmgm@gmail.com>
2021-12-30 18:41:46 +0530

    added blog with comments

---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get articles_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get articles_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get articles_edit_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get articles_show_url
+    assert_response :success
+  end
+
+  test "should get _form" do
+    get articles__form_url
+    assert_response :success
+  end
+end
